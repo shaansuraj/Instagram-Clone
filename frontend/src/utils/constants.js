bf36434d141880b4d32f0f82473696a45c4700f0
shaansuraj <90745657+shaansuraj@users.noreply.github.com>
2022-09-18 18:17:32 +0530

    Add files via upload

---
+++ frontend/src/utils/constants.js
@@ -0,0 +1,65 @@
+// export const SOCKET_ENDPOINT = "http://localhost:4000";
+export const SOCKET_ENDPOINT = "https://instagrammern.herokuapp.com";
+
+export const stories = [
+    {
+        title: "JavaScript",
+        image: "javascript"
+    },
+    {
+        title: "Node.js",
+        image: "nodejs"
+    },
+    {
+        title: "Express.js",
+        image: "expressjs"
+    },
+    {
+        title: "MongoDB",
+        image: "mongodb"
+    },
+    {
+        title: "React.js",
+        image: "reactjs"
+    },
+    {
+        title: "Socket.io",
+        image: "socketio"
+    },
+    {
+        title: "TailwindCSS",
+        image: "tailwind"
+    },
+    {
+        title: "Heroku",
+        image: "heroku"
+    },
+    {
+        title: "MaterialUI",
+        image: "mui"
+    },
+    {
+        title: "Redux",
+        image: "redux"
+    },
+    {
+        title: "Multer",
+        image: "multer"
+    },
+    {
+        title: "AWS S3",
+        image: "aws-s3"
+    },
+    {
+        title: "Sendgrid",
+        image: "sendgrid"
+    },
+    {
+        title: "Axios",
+        image: "axios"
+    },
+    {
+        title: "Toastify",
+        image: "toastify"
+    },
+]
